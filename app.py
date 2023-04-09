634453669a4786141429406df02daf94dd91f5e4
Sahil-choudhary <32207288+hksahil@users.noreply.github.com>
2023-04-09 17:54:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import zipfile
 import io
+import json
 
 st.title('Automate your PBIX file')
 
@@ -25,25 +26,40 @@ if ss:
                 if name == 'Report/Layout':
                     # Read the contents of the layout file
                     data = source_zip.read(name).decode('utf-16 le')
+                    print(data,'data')
 
+                    # Old layout file
+                    with open('og_file.json', 'w') as f:
+                            a=json.loads(data)
+                            json.dump(a, f)
                     try:
                     # Loop through each section
+                        data=json.loads(data)
                         for section in data['sections']:
+                            print(section)
+
                             # Loop through each visualContainer
                             for visualContainer in section['visualContainers']:
                                 # Check if y is 0 and config contains "parallelogram"
-                                if "parallelogram" in visualContainer['config'] :
+                                if visualContainer['y'] == 0 and ( "parallelogram" in visualContainer['config'] or "rectangle" in visualContainer['config']) :
                                     # Remove the visualContainer
                                     section['visualContainers'].remove(visualContainer)
+                        
+                        # new layout file
+                        with open('updated_file.json', 'w') as f:
+                            json.dump(data, f)
+                    
                     except:
                         print('hi')
                     # Add the manipulated layout data to the destination zip file
+                    data = json.dumps(data)
                     destination_zip.writestr(name, data.encode('utf-16 le'))
                 
                 else:
                     # Add the file to the destination zip file as-is
                     binary_data = source_zip.read(name)
                     destination_zip.writestr(name, binary_data)
+
 
     # Download the destination file
     st.download_button(
